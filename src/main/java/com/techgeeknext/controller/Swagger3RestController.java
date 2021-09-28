b5aa04cf13746640aba1c811999a7e55f4d96050
TechGeekNeXt <techgeeknext@gmail.com>
2021-09-28 18:19:17 +0530

    techgeeknext

--- src/main/java/com/techgeeknext/controller/Swagger3RestController.java
+++ src/main/java/com/techgeeknext/controller/Swagger3RestController.java
@@ -3,6 +3,7 @@ package com.techgeeknext.controller;
 import com.techgeeknext.model.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -22,17 +23,17 @@ public class Swagger3RestController {
 	}
 
 
-	@RequestMapping(value = "/getUsers")
+	@RequestMapping(value = "/getUsers", method = RequestMethod.GET, produces = "application/json")
 	public List<User> getUsers() {
 		return users;
 	}
 
-	@RequestMapping(value = "/getUser/{id}")
+	@RequestMapping(value = "/getUser/{id}", method = RequestMethod.GET, produces = "application/json")
 	public User getUserById(@PathVariable(value = "id") int id) {
 		return users.stream().filter(x -> x.getId()==(id)).collect(Collectors.toList()).get(0);
 	}
 
-	@RequestMapping(value = "/getUser/role/{role}")
+	@RequestMapping(value = "/getUser/role/{role}", method = RequestMethod.GET, produces = "application/json")
 	public List<User> getUserByRole(@PathVariable(value = "role") String role) {
 		return users.stream().filter(x -> x.getRole().equalsIgnoreCase(role))
 				.collect(Collectors.toList());
